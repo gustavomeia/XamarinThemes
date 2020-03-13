5faeecf1ee59c817f9db33115a3c8ce47cb351ec
Gustavo Pereira de Freitas <gustavo.freitas@arctouch.com>
2020-03-13 11:55:27 -0300

    Handle device theme

--- XamarinThemes/App.xaml.cs
+++ XamarinThemes/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Xamarin.Forms;
+﻿using Xamarin.Essentials;
+using Xamarin.Forms;
+using XamarinThemes.Styles;
 
 namespace XamarinThemes
 {
@@ -11,8 +13,11 @@ namespace XamarinThemes
             MainPage = new MainPage();
         }
 
+        public static AppTheme ActualTheme { get; set; } = AppTheme.Unspecified;
+
         protected override void OnStart()
         {
+            SetAppTheme();
         }
 
         protected override void OnSleep()
@@ -21,6 +26,39 @@ namespace XamarinThemes
 
         protected override void OnResume()
         {
+            SetAppTheme();
+        }
+
+        // Aplica o tema ao app de acordo com o tema solicitado pelo sistema
+        private void SetAppTheme()
+        {
+            // Verifica se o tema do sistema é o Dark
+            if (AppInfo.RequestedTheme == AppTheme.Dark)
+            {
+                // Verifica se o tema atual do nosso app já é o solicitado pelo sistema
+                if (ActualTheme == AppTheme.Dark)
+                {
+                    return;
+                }
+
+                // Aplica o tema Dark ao nosso aplicativo
+                Current.Resources = new DarkTheme();
+                // Altera nossa propriedade para refletir o tema atual do nosso aplicativo
+                ActualTheme = AppTheme.Dark;
+            }
+            else // Se não for o tema Dark vamos aplicar o tema Light ao nosso aplicativo
+            {
+                // Verifica se o tema atual do nosso app já é o solicitado pelo sistema
+                if (ActualTheme == AppTheme.Light)
+                {
+                    return;
+                }
+
+                // Aplica o tema Light ao nosso aplicativo
+                Current.Resources = new LightTheme();
+                // Altera nossa propriedade para refletir o tema atual do nosso aplicativo
+                ActualTheme = AppTheme.Light;
+            }
         }
     }
 }
